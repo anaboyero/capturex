036121010a6e890ef665960c3dcf104e99096712
anaboyero <anaboyero@gmail.com>
2026-02-22 15:23:38 +0100

    Get description and lessonLearned data, slice 3

--- extension/popup.js
+++ extension/popup.js
@@ -1,8 +1,24 @@
 chrome.tabs.query(
   { active: true, currentWindow: true },
   function (tabs) {
-    const url = tabs[0].url;
 
+    const url = tabs[0].url;
     document.getElementById("url").textContent = url;
+
   }
 );
+
+// botón
+document.getElementById("createBtn")
+  .addEventListener("click", () => {
+
+    const description =
+      document.getElementById("description").value;
+
+    const lessonLearned =
+      document.getElementById("lessonLearned").value;
+
+    console.log("Artifact data:");
+    console.log(description);
+    console.log(lessonLearned);
+  });
